734995f0bce1c109a6256e25bb5c237637dcb4a0
KrishnaUpadhyay04 <kupadhyay2004@gmail.com>
2026-09-14 10:29:54 +0530

    UC1

---
+++ AddressBookApp/Models/Contact.cs
@@ -0,0 +1,32 @@
+namespace AddressBookApp.Models;
+
+public class Contact
+{
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public string Address {get; set;}
+    public string City {get; set;}
+    public string State {get; set;}
+    public string Zip {get; set;}
+    public string PhoneNumber {get; set;}
+    public string Email {get; set;}
+
+
+    public Contact(string FirstName, string LastName, string Address, string City, string State, string Zip, string PhoneNumber, string Email)
+    {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Address = Address;
+        this.City = City;
+        this.State = State;
+        this.Zip = Zip;
+        this.PhoneNumber = PhoneNumber;
+        this.Email = Email;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} | {Address}, {City}, {State} {Zip} | {PhoneNumber} | {Email}";
+    }
+}
---
+++ AddressBookApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using AddressBookApp.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Contact contact = new Contact("Krishna", "Upadhyay", "123", "Gurugram", "Haryana", "122001", "1111111111", "krishna0636.be23@chitkara.edu.in");
+
+        string contactInfo = contact.ToString();
+        Console.WriteLine(contactInfo);
+    }
+}
